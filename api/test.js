2757e6cf8f61da9cc5127fc57e9975d0cde1d533
smolkalwdz <smolkalowanidzequez@gmail.com>
2025-08-08 13:53:37 +0400

    Delete api/test.js

--- api/test.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res) => {
-  res.json({ 
-    message: 'Test endpoint working!',
-    method: req.method,
-    url: req.url,
-    timestamp: new Date().toISOString()
-  });
-}; 
